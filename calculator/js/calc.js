1a8f74692047a3df7ae4e86d6a8b13e1f952c62c
yarik_000 <yarik.red@gmail.com>
2015-12-07 17:10:07 +0200

    adding calculator to main page, use html import

---
+++ calculator/js/calc.js
@@ -0,0 +1,7 @@
+/**
+ * Created by yarik_000 on 10.11.2014.
+ */
+var finalCost ;
+function calculation() {
+    document.getElementById("printCost").childNodes[0].nodeValue = finalCost + " грн";
+}
---
+++ calculator/js/ui.js
@@ -0,0 +1,13 @@
+/**
+ * Created by yarik_000 on 10.11.2014.
+ */
+$(document).ready(function () {
+
+    //change colors format paper
+    $('.format-paper').click(function (){
+        $('div.format-paper').css('background-color', '#E1EEF5');
+        $(this).css('background-color', '#08c');
+       // alert(this);
+        //return;
+    });
+});
